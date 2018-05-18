0ecd707b692fa803d8ce71f569220df7a22a2e32
huangmengxuan <huangmengxuan@cheweiguanjia.com>
2018-05-18 21:41:10 +0800

    初始化项目

---
+++ DYTest/DYTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYTest
+//
+//  Created by Mention7 on 2018/5/18.
+//  Copyright © 2018年 Mention7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYTest/DYTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYTest
+//
+//  Created by Mention7 on 2018/5/18.
+//  Copyright © 2018年 Mention7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
